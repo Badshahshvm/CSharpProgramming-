08bde1e26bcc9ec59a0f72dc888f4a359a1b0c1e
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2025-07-09 15:36:37 +0530

    Create SingleInheritance.cs

---
+++ SingleInheritance.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Animal
+{
+    public void Eat() => Console.WriteLine("Animal is eating...");
+}
+
+class Dog : Animal
+{
+    public void Bark() => Console.WriteLine("Dog is barking...");
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Dog d = new Dog();
+        d.Eat();
+        d.Bark();
+    }
+}
