6df349d95f477010914b56a19159db1536a8d665
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2025-07-15 17:24:46 +0530

    Create ArrayList.cs

---
+++ ArrayList.cs
@@ -0,0 +1,38 @@
+
+//First Why we need arraylist:-
+
+class Program{
+  static void Main(string[], args)
+  {
+    int []a=new int[3];
+    a[0]=2;
+    a[1]=3;
+    a[2]=5;
+    Array.resize(ref a,4);
+    a[3]=8;
+  }
+  }
+
+//Basics Of arrayList
+
+using System;
+using System.Collections;
+
+class HelloWorld {
+  static void Main() {
+    ArrayList l1 = new ArrayList();
+
+    Console.WriteLine("Initial Capacity: " + l1.Capacity);
+
+    l1.Add(5);
+    l1.Add("shivam");
+    l1.Add('a');
+
+    Console.WriteLine("Elements in ArrayList:");
+    foreach (object sbj in l1)
+    {
+        Console.Write(sbj + " ");
+    }
+  }
+}
+
