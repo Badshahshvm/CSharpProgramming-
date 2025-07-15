3de0b5f0ad122f79087dea5f94afebad753eb5f0
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2025-07-15 17:30:23 +0530

    Update ArrayList.cs

--- ArrayList.cs
+++ ArrayList.cs
@@ -36,3 +36,104 @@ class HelloWorld {
   }
 }
 
+
+//Insert an elemnt at any specific index in an arraylist
+using System;
+using System.Collections;
+
+class HelloWorld {
+  static void Main() {
+    ArrayList l1 = new ArrayList();
+
+    Console.WriteLine("Initial Capacity: " + l1.Capacity);
+
+    l1.Add(5);
+    l1.Add("shivam");
+    l1.Add('a');
+
+    Console.WriteLine("Elements in ArrayList:");
+    foreach (object sbj in l1)
+    {
+        Console.Write(sbj + " ");
+    }
+    
+    l1.Insert(2,56);
+    Console.WriteLine();
+    foreach (object sbj in l1)
+    {
+        Console.Write(sbj + " ");
+    }
+  }
+}
+
+//REmove elemnt from arraylist
+
+using System.Collections;
+
+class HelloWorld {
+  static void Main() {
+    ArrayList l1 = new ArrayList();
+
+    Console.WriteLine("Initial Capacity: " + l1.Capacity);
+
+    l1.Add(5);
+    l1.Add("shivam");
+    l1.Add('a');
+
+    Console.WriteLine("Elements in ArrayList:");
+    foreach (object sbj in l1)
+    {
+        Console.Write(sbj + " ");
+    }
+    
+    l1.Insert(2,56);
+    Console.WriteLine();
+    foreach (object sbj in l1)
+    {
+        Console.Write(sbj + " ");
+    }
+    l1.Remove(56);
+     Console.WriteLine();
+    foreach (object sbj in l1)
+    {
+        Console.Write(sbj + " ");
+    }
+  }
+}
+
+//Remove ArrayList from any specific position in arraylist
+
+using System.Collections;
+
+class HelloWorld {
+  static void Main() {
+    ArrayList l1 = new ArrayList();
+
+    Console.WriteLine("Initial Capacity: " + l1.Capacity);
+
+    l1.Add(5);
+    l1.Add("shivam");
+    l1.Add('a');
+
+    Console.WriteLine("Elements in ArrayList:");
+    foreach (object sbj in l1)
+    {
+        Console.Write(sbj + " ");
+    }
+    
+    l1.Insert(2,56);
+    Console.WriteLine();
+    foreach (object sbj in l1)
+    {
+        Console.Write(sbj + " ");
+    }
+    l1.RemoveAt(2);
+     Console.WriteLine();
+    foreach (object sbj in l1)
+    {
+        Console.Write(sbj + " ");
+    }
+  }
+}
+
+
