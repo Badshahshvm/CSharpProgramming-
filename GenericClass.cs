f2f300688b49ff31078c8e3cb9d093fb402e3840
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2025-07-16 16:45:45 +0530

    Create GenericClass.cs

---
+++ GenericClass.cs
@@ -0,0 +1,25 @@
+//Generic Class
+
+    class Example<T>
+    {
+        T b;
+        public Example(T b)
+        {
+
+            this.b = b;
+        }
+        public  T getBox()
+        {
+            return this.b;
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#");
+            Example<int> e = new Example<int>(2);
+            Console.WriteLine(e.getBox());
+        }
+    }
